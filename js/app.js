659652512427fe2207a7f631371833dcd24f35e4
Ansgar Hoffmann <ansgar.hoffmann@daimler.com>
2021-02-10 13:49:31 +0100

    still missing the navbar active behaviour

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const menuList = document.getElementById("menulist");
 const sections = document.querySelectorAll(".section-header");
+const menuItems = [];
 
 function printSectionInfo() {
   console.log("Found " + sections.length + " sections: ");
@@ -11,12 +12,81 @@ function printSectionInfo() {
 function addSectionsToNav(sections) {
   for (section of sections) {
     let li = document.createElement("li");
+    li.setAttribute("id", "li" + section.getAttribute("id"));
+    let link = document.createElement("a");
+    link.setAttribute("href", "#" + section.getAttribute("id"));
+    link.textContent = section.getAttribute("data-navtitle");
+    link.classList.add("navbar-link");
+    link.addEventListener("click", onClickNavItem);
     li.classList.add("navbar-listitem");
-    li.textContent = section.getAttribute("data-navtitle");
+    li.appendChild(link);
     menuList.appendChild(li);
+    menuItems.push(li);
+  }
+}
+
+/* although scrolling would have worked just as well with the regular a href to id, 
+here a click handler to scroll to the section and preventing the working default behaviour */
+function onClickNavItem(event) {
+  console.log("Click " + event);
+  if (event.target != null) {
+    event.target.scrollToLocation();
+    event.preventDefault();
+  }
+}
+
+// copied and modified version of: https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport
+function isElementPartiallyInViewport(el) {
+  const waitFromTop = 150;
+  const waitFromBottom = 250;
+  var rect = el.getBoundingClientRect();
+  // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+  var windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  var vertInView =
+    rect.top + waitFromTop <= windowHeight &&
+    rect.top + rect.height >= waitFromBottom;
+  var horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
+
+  return vertInView && horInView;
+}
+
+/* highlight section by adding a class if the element is in viewport (functions copied from stackoverflow) */
+function onScrollHighlightActiveSection() {
+  // console.log("scroll event received");
+  // for (section of sections) {
+  //   const navbarElement = document.querySelector(
+  //     "#li" + section.getAttribute("id")
+  //   );
+  //   if (isElementPartiallyInViewport(section.parentElement)) {
+  //     section.parentElement.classList.add("section-currentlyactive");
+  //     navbarElement.classList.add("navbar-listitem-active");
+  //   } else {
+  //     section.parentElement.classList.remove("section-currentlyactive");
+  //   }
+  // }
+
+  // use conventional for loop to be able to use the index for selection of the navbar Elements;
+  for (let index = 0; index < sections.length; index++) {
+    // const navbarElement = document.querySelector(
+    //   "#li" + sections[index].getAttribute("id")
+    // );
+    if (isElementPartiallyInViewport(sections[index].parentElement)) {
+      sections[index].parentElement.classList.add("section-currentlyactive");
+      menuItems[index].classList.add("navbar-listitem-active");
+    } else {
+      sections[index].parentElement.classList.remove("section-currentlyactive");
+      menuItems[index].classList.remove("navbar-listitem-active");
+    }
   }
 }
 
 printSectionInfo();
 
 addSectionsToNav(sections);
+
+/* execute once to highlight the currently visible section, then register scroll listener */
+onScrollHighlightActiveSection();
+window.onscroll = onScrollHighlightActiveSection;
